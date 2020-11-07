cdd2f6b141b2ca1ce5833ea526d29208a21b0b5c
ikyriazis <ikyriazis@wpi.edu>
2020-11-07 14:46:38 -0500

    finished login/signup backend stuff

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(session({ secret: "cats" }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -53,7 +54,34 @@ async function addUser(fname, lname, user, pass) {
     await client.shutdown();
 }
 
+// given a username and password, this function checks the database for the username
+// and compares both to see if they match. returns true if they match, false if they don't
+let presentaccountid;
+let presentaccountuser;
+async function authenticateUser(user, pass) {
+    const client = new Client({
+        cloud: { secureConnectBundle: 'secure-connect-whack.zip' },
+        credentials: { username: 'user', password: 'pass' },
+        keyspace: 'whack'
+    });
 
+    await client.connect();
+
+    const select = 'select * from users where user = :user';
+    const params = [user];
+
+    let result = await client.execute(select, params);
+
+    const userResult = result.first();
+    if (user === userResult.user && pass === userResult.pass) {
+        presentaccountid = userResult.id;
+        presentaccountuser = userResult.user;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -73,15 +101,23 @@ app.get('/signup', (req, res) => {
     }
 });
 
+
 // the post request end point to log into an account
-app.get('/login', (req, res) => {
+app.get('/login', async (req, res) => {
     let user = req.body.user;
     let pass = req.body.pass;
-    if (!userExists(user)) {
+    if (! await userExists(user)) {
         res.json({message: 'Username not found in database'});
     }
-    else {
+    else if (await authenticateUser(user, pass)) {
+        req.session.accountSession = presentaccountid;
+        req.session.auth = true;
+        req.session.username = presentaccountuser;
 
+        res.json({message: 'User is logged in'});
+    }
+    else {
+        res.json({message: 'Password didn\'t match'});
     }
 })
 
